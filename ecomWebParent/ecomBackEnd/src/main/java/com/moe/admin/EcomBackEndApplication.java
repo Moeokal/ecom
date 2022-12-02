97b63eb373aea73dd4f8feea6a3a2c32af621b16
Moeokal <moayadokal1@gmail.com>
2022-12-02 20:14:12 +0100

    first commit

---
+++ ecomWebParent/ecomBackEnd/src/main/java/com/moe/admin/EcomBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.moe.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomBackEndApplication.class, args);
+    }
+
+}
---
+++ ecomWebParent/ecomFrontEnd/src/main/java/com/moe/site/EcomFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.moe.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomFrontEndApplication.class, args);
+    }
+
+}
