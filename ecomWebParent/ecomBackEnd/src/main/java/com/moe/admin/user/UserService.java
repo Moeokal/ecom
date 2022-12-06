838158a19d54f7a899fdf2872c842658a31e9dcc
Moeokal <moayadokal1@gmail.com>
2022-12-07 00:04:51 +0100

    add Users listing function

---
+++ ecomWebParent/ecomBackEnd/src/main/java/com/moe/admin/user/UserService.java
@@ -0,0 +1,19 @@
+package com.moe.admin.user;
+
+import com.moe.ecomcommon.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> listAll(){
+        return (List<User>) repo.findAll();
+    }
+
+}
