78dfe7931ac95eb34e1aa0c8939480d482228f1f
Moeokal <moayadokal1@gmail.com>
2022-12-07 23:43:12 +0100

    add create user functionality

--- ecomWebParent/ecomBackEnd/src/main/java/com/moe/admin/user/UserService.java
+++ ecomWebParent/ecomBackEnd/src/main/java/com/moe/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.moe.admin.user;
 
+import com.moe.ecomcommon.entity.Role;
 import com.moe.ecomcommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,19 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+    @Autowired
+    private RoleRepository roleRepo;
 
     public List<User> listAll(){
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepo.findAll();
     }
 
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepo.findAll();
+    }
+
+    public void save(User user) {
+        userRepo.save(user);
+    }
 }
